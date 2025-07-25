f3fd819d557166ae108bfe149043a30aa99551a1
ShyRed <ShyRed@users.noreply.github.com>
2025-07-25 18:56:31 +0200

    Add TreeDataGrid with data loading and hierarchical structure handling to TreePageView

--- AvaloniaTreeComboTest.Core/ViewModels/TreePageViewModel.cs
+++ AvaloniaTreeComboTest.Core/ViewModels/TreePageViewModel.cs
@@ -1,6 +1,52 @@
-﻿namespace AvaloniaTreeComboTest.Core.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaTreeComboTest.Core.ViewModels;
 
 public partial class TreePageViewModel : ViewModelBase
 {
+    private const int MaxItems = 100000;
     
+    [ObservableProperty] private ObservableCollection<TreeItem> _items = [];
+
+    [RelayCommand]
+    private async Task LoadItemsAsync()
+    {
+        Items = await Task.Run(() =>
+        {
+            var items = new List<TreeItem>(MaxItems);
+            var parent = new TreeItem("First Entry", 0, []);
+            for (var i = 1; i < MaxItems; i++)
+            {
+                if (i % 1000 == 0 || i == MaxItems - 1)
+                {
+                    items.Add(parent);
+                    parent = new TreeItem($"Parent {i}", i, []);
+                }
+                else
+                    parent.Children!.Add(new TreeItem($"Child {i}", i));
+            }
+            return new ObservableCollection<TreeItem>(items);
+        });
+    }
+}
+
+public partial class TreeItem : ObservableObject
+{
+    [ObservableProperty] private string _name;
+    [ObservableProperty] private int _value;
+    [ObservableProperty] private ObservableCollection<TreeItem>? _children;
+
+    public TreeItem(string name, int value)
+    {
+        _name = name;
+        _value = value;
+    }
+
+    public TreeItem(string name, int value, ObservableCollection<TreeItem>? children)
+        : this(name, value)
+    {
+        _children = children;
+    }
 }
--- AvaloniaTreeComboTest/Views/TreePageView.axaml.cs
+++ AvaloniaTreeComboTest/Views/TreePageView.axaml.cs
@@ -1,13 +1,43 @@
-﻿using Avalonia;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using AvaloniaTreeComboTest.Core.ViewModels;
+using DynamicData.Binding;
+using ReactiveUI;
 
 namespace AvaloniaTreeComboTest.Views;
 
 public partial class TreePageView : UserControl
 {
+    private TreePageViewModel ViewModel => (TreePageViewModel)DataContext!;
+
     public TreePageView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        
+        this.WhenAnyValue(x => x.ViewModel.Items)
+            .Subscribe(x =>
+            {
+                HierarchicalTreeDataGridSource<TreeItem> source = new(ViewModel.Items);
+                source.Columns.Add(new HierarchicalExpanderColumn<TreeItem>(
+                    new TextColumn<TreeItem, string>(
+                        "Name",
+                        item => item.Name,
+                        options: new TextColumnOptions<TreeItem>() { IsTextSearchEnabled = true}),
+                    item => item.Children) );
+                source.Columns.Add(new TextColumn<TreeItem,int>(
+                    "Value",
+                    item => item.Value));
+                TreeCtrl.Source = source;
+            });
     }
 }
