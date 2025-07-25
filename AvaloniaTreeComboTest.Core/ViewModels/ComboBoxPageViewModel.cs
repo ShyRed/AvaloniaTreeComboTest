9fbc622a8a67f8c4c522bfad73026bb41353ada7
ShyRed <ShyRed@users.noreply.github.com>
2025-07-25 16:05:58 +0200

    Add ComboBox with data loading functionality to ComboBoxPageView

--- AvaloniaTreeComboTest.Core/ViewModels/ComboBoxPageViewModel.cs
+++ AvaloniaTreeComboTest.Core/ViewModels/ComboBoxPageViewModel.cs
@@ -1,6 +1,34 @@
-﻿namespace AvaloniaTreeComboTest.Core.ViewModels;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaTreeComboTest.Core.ViewModels;
 
 public partial class ComboBoxPageViewModel : ViewModelBase
 {
+    private const int MaxItems = 100000;
     
+    [ObservableProperty] private List<ComboBoxItem> _items = [];
+
+    [ObservableProperty] private ComboBoxItem? _selectedItem;
+
+    [RelayCommand]
+    private async Task LoadItemsAsync()
+    {
+        Items = await Task.Run(() =>
+        {
+            var items = new List<ComboBoxItem>(MaxItems);
+            for (var i = 0; i < MaxItems; i++)
+            {
+                items.Add(new ComboBoxItem(i.ToString(), i));
+            }
+            return items;
+        });
+    }
+}
+
+public sealed class ComboBoxItem(string name, int value)
+{
+    public string Name { get; } = name;
+    public int Value { get; } = value;
 }
